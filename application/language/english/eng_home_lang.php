a4fa0ce00bebe1ca5c148c46e668cf3ccb303c5f
jaroenwut <jaroenwut@yahoo.com>
2018-08-07 17:58:46 +0700

    Update home.

--- application/language/english/eng_home_lang.php
+++ application/language/english/eng_home_lang.php
@@ -1,3 +1,5 @@
 <?php
 $lang['home_browser_title'] = "Lexgazy Thinks";
+$lang['home_navbar_menu_home'] = "Home";
+$lang['home_navbar_menu_signin'] = "Sign In";
 ?>
---
+++ application/views/apphome/navbar.php
@@ -0,0 +1,19 @@
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-6 col-md-6 col-lg-6" id="brand">            
+				<a href="<?php echo base_url();?>">
+					<img src="<?php echo base_url();?>theme/shared/images/logo.png" alt="" />
+				</a>				
+			</div>		
+			<div class="col-sm-6 col-md-6 col-lg-6" id="ddnav">	
+				<ul class="nav navbar-top-links navbar-right">						
+                    <li>
+                        <a href="<?=base_url();?>"><p class="font-navbar"><?=lang('home_navbar_menu_home');?></p></a>
+                    </li>					
+                    <li>
+                        <a href="#"><p class="font-navbar"><i class="fas fa-key"></i> <?=lang('home_navbar_menu_signin');?></p></a>
+                    </li>					
+				</ul>		
+			</div>
+		</div>
+	</div>
