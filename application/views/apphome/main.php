7f1fe4a783e836e83fe0d262c28406c012e38ff0
jaroenwut <jaroenwut@yahoo.com>
2018-08-10 02:18:36 +0700

    update all star vote.

--- application/views/apphome/main.php
+++ application/views/apphome/main.php
@@ -167,9 +167,9 @@
 								<div class="col-xs-6 col-lg-6">
 									<i class="fa fa-star icon-orange" data-rating="1"></i>
 									<i class="fa fa-star icon-orange" data-rating="2"></i>
-									<i class="fa fa-star-o icon-orange" data-rating="3"></i>
-									<i class="fa fa-star-o icon-orange" data-rating="4"></i>
-									<i class="fa fa-star-o icon-orange" data-rating="5"></i>	
+									<i class="fa fa-star icon-orange" data-rating="3"></i>
+									<i class="fa fa-star icon-orange" data-rating="4"></i>
+									<i class="fa fa-star icon-orange" data-rating="5"></i>	
 								</div>		
 								<div class="col-xs-6 col-lg-6 text-right">
 									<p class="font-flexmin">1 Votes</p>
